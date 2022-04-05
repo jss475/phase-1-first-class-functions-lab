73605fedd9e9b5e7223f3b51200a28b602051e4b
JosephShin <joespam9999@gmail.com>
2022-04-05 10:09:27 -0400

    commit tree 0e21894ab5df00410348e990911661c6a0fbbe97

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+/*function returnFirstTwoDrivers(param = ['Antonia','Nuru','diver3']){
+    let newArray = param.slice(0,2);
+    return newArray;
+}*/
+
+const returnFirstTwoDrivers = (function(param = ['Antonia','Nuru','diver3']) {
+    let newArray = param.slice(0,2);
+    return newArray;
+});
+
+const returnLastTwoDrivers = function(param = ['driver1','driver2','driver3']){
+    let newArray = param.slice(param.length-2,param.length);
+    return newArray;
+}
+
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(mult){
+    return function(fare){
+        return fare*mult;
+    }
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(param = ['driver1','driver2','driver3','driver4'],selectingDrivers){
+    return selectingDrivers(param)
+}
